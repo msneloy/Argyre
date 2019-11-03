49ff5e8e8c51c3f3ed16b777728963d3ca5862d5
Mahadi Sajjad Neloy <sierra117@qq.com>
2019-11-03 14:12:35 +0800

    Create pinecraft.py

---
+++ pinecraft.py
@@ -0,0 +1,84 @@
+commands = {
+                "i"            : "see inventory",
+                "c"            : "see crafting options",
+                "craft [item]" : "craft something from inventory items",
+           }
+#an inventory of items
+items = {
+            "flint"   : 50,
+            "grass"   : 100,
+            "hay"     : 0,
+            "tree"    : 100,
+            "log"     : 0,
+            "sapling" : 100,
+            "twig"    : 0,
+            "boulder" : 30,
+            "rock"    : 0,
+            "pickaxe" : 0,
+            "axe"     : 0,
+            "firepit" : 0,
+            "tent"    : 0,
+            "torch"   : 0,
+        }
+#rules to make new objects
+craft = {
+            "hay"     : { "grass"   : 1                                          },
+            "twig"    : { "sapling" : 1                                          },
+            "log"     : { "axe"     : 1,  "tree"  : 1                            },
+            "axe"     : { "twig"    : 3,  "flint" : 1                            },
+            "tent"    : { "twig"    : 10, "hay"   : 15                           },
+            "firepit" : { "boulder" : 5,  "log"   : 3,  "twig" : 1,  "torch" : 1 },
+            "torch"   : { "flint"   : 1,  "grass" : 1,  "twig" : 1               },
+            "pickaxe" : { "flint"   : 2,  "twig"  : 1                            }
+        }
+print("PineCraft by Alec-Neloy-Tayef")
+print("-----------------------------")
+print("You're lost in The Bongo forest of Bonga Bonga Island.")
+print("It's going to be night soon,and you need to secure a base before night.")
+print("Make a Tent and a Firepit to survive the night!")
+print("Type '?' to inspect system calls")
+while True:
+    command = input(">").split()
+    if len(command) == 0:
+        continue
+    if len(command) > 0:
+        verb = command[0].lower()
+    if len(command) > 1:
+        item = command[1].lower()
+    if verb == "?":
+        for key in commands:
+            print(key + " : " + commands[key])
+        print("\n")
+    elif verb == "i":
+        for key in items:
+            print(key + " : " + str(items[key]))
+        print("\n")
+    elif verb == "c":
+        for key in craft:
+            print(key + " can be made with:")
+            for i in craft[key]:
+                print(str(craft[key][i]) + " " + i)
+            print("\n")
+    elif verb == "craft":
+        print("making " + item + ":")
+        if item in craft:
+            for i in craft[item]:
+                print("  you need : " + str(craft[item][i]) + " " + i + " and you have " + str(items[i]))
+            canBeMade = True
+            for i in craft[item]:
+                if craft[item][i] > items[i]:
+                    print("item cannot be crafted\n")
+                    canBeMade = False
+                    break
+            if canBeMade == True:
+                for i in craft[item]:
+                    items[i] -= craft[item][i]
+                items[item] += 1
+                print("item crafted\n")
+            if items["tent"] >= 1 and items["firepit"] >= 1:
+                print("\n**Well,you've managed to escape Bonga Bonga.........for now.\nWELL DONE!")
+                break
+        else:
+            print("you can't")
+    else:
+        print("you can't")
